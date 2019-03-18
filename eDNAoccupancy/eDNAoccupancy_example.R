fd33f5edf586b4894cadb26514d29d024cb46f10
Noemi Krisztina Nagy <noemikrisztinanagy@Old-Mac.local>
2019-03-18 13:26:52 +0100

    dates

--- eDNAoccupancy/eDNAoccupancy_example.R
+++ eDNAoccupancy/eDNAoccupancy_example.R
@@ -1,4 +1,5 @@
 library(eDNAoccupancy)
+library(tidyverse)
 
 setwd("~/Google Drive/Lake diversity/Lake_diversity_Prepared/Analyses Lake Div/workdir/")
 rm(list=ls())
@@ -7,3 +8,11 @@ load(file="~/Google Drive/Lake diversity/Lake_diversity_Prepared/Analyses Lake D
 # example OTU
 most_common_otu <- data.frame(most_common = final_data$final_otus[,"HISEQ:267:CAJCDANXX:2:1101:13278:2098_CONS_SUB_SUB"])
 rownames(most_common_otu) <-rownames(final_data$final_otus)
+
+# link dates to depths
+most_common_otu$Horiz <- sapply(rownames(most_common_otu), 
+                                function(x) strsplit(x,"_")[[1]][1])
+
+most_common_otu %>%
+  left_join(final_data$proxies, by = c("Horiz" = "dna")) %>%
+  select(most_common, date_final)
